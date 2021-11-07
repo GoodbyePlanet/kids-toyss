90f7d9753e1fdea829867f4c334230039d6d66cb
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 04:35:40 +0100

    Fix repository

--- src/main/java/com/kidstoys/kidstoys/repository/UserRepository.java
+++ src/main/java/com/kidstoys/kidstoys/repository/UserRepository.java
@@ -9,6 +9,6 @@ import com.kidstoys.kidstoys.domain.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
