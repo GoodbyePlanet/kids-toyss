20ae0efcda68f18288794cfd1f5271d61886f1fa
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 04:34:57 +0100

    Revert "Change to UUID"
    This reverts commit babf092a9bfdd92946667c4a035c669574147387.

--- src/main/java/com/kidstoys/kidstoys/domain/User.java
+++ src/main/java/com/kidstoys/kidstoys/domain/User.java
@@ -1,15 +1,10 @@
 package com.kidstoys.kidstoys.domain;
 
-import java.util.UUID;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,11 +20,8 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
-    @GeneratedValue(generator = "UUIDGenerator")
-    @Type(type = "pg-uuid")
-    @Column(name = "id", updatable = false, nullable = false)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
     private String name;
 
