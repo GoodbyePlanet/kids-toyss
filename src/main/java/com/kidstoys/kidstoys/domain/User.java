babf092a9bfdd92946667c4a035c669574147387
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 03:48:13 +0100

    Change to UUID

--- src/main/java/com/kidstoys/kidstoys/domain/User.java
+++ src/main/java/com/kidstoys/kidstoys/domain/User.java
@@ -1,5 +1,7 @@
 package com.kidstoys.kidstoys.domain;
 
+import java.util.UUID;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,7 +23,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private UUID id;
 
     private String name;
 
