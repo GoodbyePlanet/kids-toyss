69eeb9b25dd40e8a7b7d667e1b7b16a25ea01097
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 04:30:38 +0100

    Fix migration script

--- src/main/resources/db/changelog/changes/001-add-user-table.sql
+++ src/main/resources/db/changelog/changes/001-add-user-table.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS "user"
 (
-    id uuid DEFAULT uuid_generate_v4()
+    id uuid DEFAULT uuid_generate_v4(),
     name  varchar(50)  NOT NULL,
     email varchar(255)  NOT NULL,
     primary key (id)
