74cee8bc651521c1035ae9a610c3fcd585f297b1
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 11:45:55 +0100

    Add WebConfig

--- target/classes/db/changelog/changes/001-add-user-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS "user"
-(
-    id uuid DEFAULT uuid_generate_v4(),
-    name  varchar(50)  not null,
-    email varchar(255)  not null,
-    primary key (id)
-);
