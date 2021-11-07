988484be8ae0a01336e1f9e2d2bf12f377efe5c0
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 04:29:03 +0100

    Add uuid-ossp

--- src/main/resources/db/changelog/changes/001-add-user-table.sql
+++ src/main/resources/db/changelog/changes/001-add-user-table.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS "user"
 (
-    id uuid NOT NULL,
+    id uuid DEFAULT uuid_generate_v4()
     name  varchar(50)  NOT NULL,
     email varchar(255)  NOT NULL,
     primary key (id)
