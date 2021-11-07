9c6cdea537ab2b70325d78ecbffe36e5319793c6
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 04:25:33 +0100

    Add type for id

--- src/main/java/com/kidstoys/kidstoys/domain/User.java
+++ src/main/java/com/kidstoys/kidstoys/domain/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,6 +27,7 @@ public class User {
     @Id
     @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
     @GeneratedValue(generator = "UUIDGenerator")
+    @Type(type = "pg-uuid")
     @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
 
