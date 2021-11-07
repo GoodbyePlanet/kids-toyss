f06481e87385a800e0059e7e30086ae3b219163e
Nemanja <nemanja.vasic@codecentric.de>
2021-11-07 04:35:16 +0100

    Revert migration script

--- src/main/resources/db/changelog/changes/001-add-user-table.sql
+++ src/main/resources/db/changelog/changes/001-add-user-table.sql
@@ -2,8 +2,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS "user"
 (
-    id uuid DEFAULT uuid_generate_v4(),
-    name  varchar(50)  NOT NULL,
-    email varchar(255)  NOT NULL,
-    primary key (id)
+    id bigserial NOT NULL,
+    name  VARCHAR(50)  NOT NULL,
+    email VARCHAR(255)  NOT NULL,
+    PRIMARY KEY (id)
 );
